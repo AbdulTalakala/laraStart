07a745592f3c7d138b6c4fc3b04f17adc77393a9
Abdul <khader.abdulm@gmail.com>
2019-02-17 23:05:36 +0400

    lara sratrt new

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -9,8 +8,67 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import { Form, HasError, AlertError } from 'vform'
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import Dashboard from './components/Dashboard.vue'
+import Profile from './components/Profile.vue'
+import Users from './components/Users'
+import moment from 'moment'
+import VueProgressBar from 'vue-progressbar'
+
+import Swal from 'sweetalert2'
+window.Swal  = Swal;
+
+const toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+window.toast = toast;
+
+Vue.use('VueProgressBar',{
+    color:'rgb(143,255,199)',
+    failedColor:'red',
+    height: '4px'
+})
+
+let routes = [
+    {path: '/dashboard',component: Dashboard},
+    {path:'/profile',component: Profile},
+    {path:'/users',component: Users}
+]
+// const routes = [
+//     { path: '/foo', component:  ExampleComponent},
+//     { path: '/bar', component: Bar }
+// ]
+
+
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for routes: routes h
+})
+
+Vue.filter('capitalText',function(text){
+   return text.charAt(0).toUpperCase() + text.slice(1)
+});
+
+Vue.filter('myDate',function(created_at){
+   return moment(created_at).format('MMMM DD YYYY');
+});
+
+
+
 /**
- * The following block of code may be used to automatically register your
+ * The following block of code  be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
@@ -21,6 +79,10 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('dashboard', require('./components/Dashboard.vue'));
+//Vue.component('profile', require('./components/Profile.vue'));
+
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,5 +91,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router,
+
 });
